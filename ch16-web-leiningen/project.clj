21e7ae9d55c67bc1fe349e508336dd2b11b884d0
Simon Katz <git@nomistech.com>
2013-04-11 14:41:34 +0100

    Tidy indentation.

--- ch16-web-leiningen/project.clj
+++ ch16-web-leiningen/project.clj
@@ -2,6 +2,6 @@
   :description "A toy URL shortener HTTP service written using Ring and Compojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [compojure "1.0.1"]
-		 		         [ring "1.0.1"]]
+                 [ring "1.0.1"]]
   :plugins [[lein-beanstalk "0.2.2"]]
   :ring {:handler com.clojurebook.url-shortener.beanstalk/app})

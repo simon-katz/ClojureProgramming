67bd7d904ae94a7c119897d72169d7babfbae64e
Simon Katz <git@nomistech.com>
2013-05-02 15:45:24 +0100

    Change to depend on [org.clojure/clojure "1.5.1"].  (With 1.3.0, nrepl-jack-in from Emacs was failing.)

--- ch16-web-leiningen/project.clj
+++ ch16-web-leiningen/project.clj
@@ -1,6 +1,6 @@
 (defproject com.clojurebook/url-shortener "1.0.0-SNAPSHOT"
   :description "A toy URL shortener HTTP service written using Ring and Compojure."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.0.1"]
                  [ring "1.0.1"]]
   :plugins [[lein-beanstalk "0.2.2"]]

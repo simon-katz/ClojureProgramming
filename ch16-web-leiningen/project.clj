88e2ad0c8e1bf774fb4a9c7a19c5a8d4324a3940
Simon Katz <git@nomistech.com>
2013-05-02 16:34:55 +0100

    A file of curl commands.  Upgraded to Compojure 1.1.5 and Ring 1.1.8.  Use Lein Ring.

--- ch16-web-leiningen/project.clj
+++ ch16-web-leiningen/project.clj
@@ -1,7 +1,9 @@
 (defproject com.clojurebook/url-shortener "1.0.0-SNAPSHOT"
   :description "A toy URL shortener HTTP service written using Ring and Compojure."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.0.1"]
-                 [ring "1.0.1"]]
-  :plugins [[lein-beanstalk "0.2.2"]]
+                 [compojure "1.1.5"]
+                 [ring "1.1.8"]]
+  :plugins [[lein-ring "0.8.5"]
+            ;;[lein-beanstalk "0.2.2"]
+            ]
   :ring {:handler com.clojurebook.url-shortener.beanstalk/app})
